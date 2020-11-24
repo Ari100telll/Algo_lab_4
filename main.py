c9fb678259d9e1c55893d56ec8dc5257310b8763
Nikita Horb <masternik007@gmail.com>
2020-11-24 20:12:47 +0200

    CHenge logic to regex

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import re
+
+
 def get_pow_arr(element, leng):
     if element == 1:
         return ["1"]
@@ -18,33 +21,27 @@ def is_all_false(arr: list):
     return True
 
 
-def find_substring(string, pow_arr: list):
+def find_substring(string, pow_strings_arr: list):
     if len(string) == 0:
         return 0
     counter = 0
-    length = len(pow_arr)
-    temp_arr = pow_arr[:]
-    false_arr = [True for i in range(length)]
-    i = 0
-    while True:
-        if is_all_false(false_arr):
-            break
-        for j in range(length):
-            if not false_arr[j]:
-                continue
-            if temp_arr[j][i] != string[i] or len(temp_arr[j]) > len(string):
-                false_arr[j] = False
-            elif i == len(temp_arr[j]) - 1:
-                steps = find_substring(string[i + 1:], pow_arr)
-                if steps != -1:
-                    if counter == 0:
-                        counter = steps
-                    counter = min(counter, steps) + 1
-                false_arr[j] = False
-        i += 1
+    for pow_string in pow_strings_arr:
+        result = re.match(pow_string, string)
+        if result is not None:
+            index = result.span(0)[1]
+            inner_counter = find_substring(string[index:], pow_strings_arr)
+            if inner_counter != -1:
+                if counter == 0:
+                    counter = inner_counter + 1
+                counter = min(counter - 1, inner_counter) + 1
     if counter == 0:
         return -1
     return counter
+
+
+def count_elements_in_string(string, element):
+    pow_arr = get_pow_arr(element, len(string))
+    return find_substring(string, pow_arr)
 
 
 if __name__ == '__main__':
@@ -52,4 +49,12 @@ if __name__ == '__main__':
     n = int(n)
     len_arr = len(s)
     n_arr = get_pow_arr(n, len_arr)
+    for a in n_arr:
+        print(a)
     print(find_substring(s, n_arr))
+
+#10000001101111110001110111000100010111101111111001111011111000000110111111000110000001101111110001 3 c: 10 sub_c: 11
+#100000011011111100011101110001000101111011
+#     1                2     3          4
+# 1110011010101001 1010001 1 11110011
+# 11100110101010011010001111110011 3
--- test_pow.py
+++ test_pow.py
@@ -3,28 +3,20 @@ from main import find_substring
 from main import get_pow_arr
 
 
-def get_pow_arr1(element, st):
-    arr = []
-    i = 0
-    while i < st:
-        arr.append(bin(element ** i)[2:])
-        i += 1
-    return arr  # [:-1]
-
-
 def generate_test_string():
     while True:
         n = randint(1, 100)
-        pow_arr = get_pow_arr1(n, 5)
+        pow_arr = get_pow_arr(n, 20)
         count = randint(1, 10)
         string = ""
+        test_s = ""
         for i in range(count):
-            string += pow_arr[randint(0, 4)]
-        # print(string + " " + str(n))
+            s = pow_arr[randint(0, len(pow_arr) - 1)]
+            string += s
+            test_s += s + " "
         sub_c = find_substring(string, get_pow_arr(n, len(string)))
-        if count != sub_c:
-            print("ERROR: " + string + " n: " + str(n) + " c: " + str(count) + " sub_c: " + str(sub_c))
-            # return 0
+        if count < sub_c or (sub_c == -1 and count != -1):
+            print("ERROR: " + test_s + " n: " + str(n) + " c: " + str(count) + " sub_c: " + str(sub_c))
 
 
 if __name__ == '__main__':
